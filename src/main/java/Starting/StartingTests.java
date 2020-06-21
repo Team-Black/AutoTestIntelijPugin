293d065eb195bd6c466180784a0b6768e2a026a8
PC <AndreyVladimirov1ch@yandex.ru>
2020-06-21 19:15:12 +0300

    Add refreshing, completed is correctly, delete unnecessary

--- src/main/java/Starting/StartingTests.java
+++ src/main/java/Starting/StartingTests.java
@@ -1,20 +1,33 @@
 package Starting;
 
 import JBSElogic.JBSEConnection;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vcs.VcsBundle;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileSystem;
+import sun.reflect.generics.tree.Tree;
 
+import javax.swing.tree.TreePath;
 import java.util.Objects;
 
 import static java.lang.Thread.sleep;
 
 public class StartingTests extends AnAction {
     JBSEConnection jbseConnection;
+
+//    public void popupInvokation() {
+//        ActionManager actionManager = ActionManager.getInstance();
+//
+//        DefaultActionGroup group = new DefaultActionGroup();
+//
+//        group.add(actionManager.getAction("createTests.AllTestsAction"));
+//
+//        final ActionPopupMenu popupMenu = ActionManager.getInstance().createActionPopupMenu(ActionPlaces.ANT_EXPLORER_POPUP, group);
+//        popupMenu.getComponent().show();
+//    }
 
     @Override
     public void actionPerformed(AnActionEvent e) {
@@ -26,10 +39,6 @@ public class StartingTests extends AnAction {
             jbseConnection.start2();
             System.out.println("Symbolic execution is completed");
         },  VcsBundle.message("command.name.open.error.message.view"), null);
-
-        //FIXME по хорошему нам надо использовать файл для проверки в цикле но как я не знаю
-//        VirtualFile  targetFile =LocalFileSystem.getInstance().
-//                findFileByPath(e.getProject().getBasePath() + jbseConnection.getTEST_NAME());
 
         VirtualFile targetPath = LocalFileSystem.getInstance().
                 findFileByPath(Objects.requireNonNull(e.getProject()).getBasePath() + "/out/");
